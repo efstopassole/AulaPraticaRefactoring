a75f9a205d99d1468a2330710e7f960e74b18168
Enzostp <efstopassole@minha.fag.edu.br>
2025-11-24 19:34:12 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -34,8 +34,6 @@ public class Movie {
         return _price.getCharge(daysRented);
     }
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
