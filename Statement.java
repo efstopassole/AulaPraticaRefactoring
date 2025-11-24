c4859ba0b4501dcd17266d68401d9e1c47fae691
Enzostp <efstopassole@minha.fag.edu.br>
2025-11-24 19:38:18 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,5 +1,18 @@
+import java.util.Enumeration;
+
 public abstract class Statement {
     public abstract String headerString(Customer aCustomer);
     public abstract String eachRentalString(Rental aRental);
     public abstract String footerString(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = headerString(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += eachRentalString(each);
+        }
+        result += footerString(aCustomer);
+        return result;
+    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,16 +1,4 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = headerString(aCustomer);
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += eachRentalString(each);
-        }
-        result += footerString(aCustomer);
-        return result;
-    }
     public String headerString(Customer aCustomer) {
         return "Rental Record for " + aCustomer.getName() + "\n";
     }
